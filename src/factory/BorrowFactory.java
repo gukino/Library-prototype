a33f527bae589a94cfcc4064267da84fffaa2bc1
LAPTOP-BP5UT6BL\Hqj <161250040@smail.nju.edu.cn>
2019-01-26 22:34:43 +0800

    add component1

--- src/factory/BorrowFactory.java
+++ src/factory/BorrowFactory.java
@@ -1,5 +1,11 @@
 package factory;
 
+import Service.BorrowStrategy;
+import Service.GraduateBorrow;
+import Service.TeacherBorrow;
+import Service.UngraduateBorrow;
+import utils.UserType;
+
 /**
  * @ClassNameBorrowFactory
  * @Describtion TODO
@@ -7,5 +13,19 @@ package factory;
  * @Version 1.0
  **/
 public class BorrowFactory {
-	
+	public static BorrowStrategy createStrategy(UserType userType){
+		BorrowStrategy borrowStrategy = null;
+		switch (userType){
+			case Teacher:
+				borrowStrategy = new TeacherBorrow();
+				break;
+			case graduate:
+				borrowStrategy = new GraduateBorrow();
+				break;
+			case Ungraduate:
+				borrowStrategy = new UngraduateBorrow();
+				break;
+		}
+		return borrowStrategy;
+	}
 }
